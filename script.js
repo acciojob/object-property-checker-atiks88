be52000d17cbc2191b5996a2f4026d2f974b8043
atiks88 <141978852+atiks88@users.noreply.github.com>
2023-10-13 12:42:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+		if (sampleObject.hasOwnProperty(key)) {
+			return true;
+		}
+		return false;
 }
-
 // Do not change the code below
-const key = prompt("Enter Key.");
+const key = prompt("Enter Key.")
 alert(hasKey(key));
